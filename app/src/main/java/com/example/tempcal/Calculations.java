17de47c21eeafaae0e651bd6c80fb3b1cefc63a6
ThameeraFernando <sithnuwan111@gmail.com>
2021-09-29 13:39:10 +0530

    Add a calculations page

---
+++ app/src/main/java/com/example/tempcal/Calculations.java
@@ -0,0 +1,14 @@
+package com.example.tempcal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Calculations extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculations);
+    }
+}
